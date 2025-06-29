9226433f9e65228ff2d20e4b543696c8f662b9b8
phamquangtrungangiang <phamquangtrungangiang@gmail.com>
2025-06-29 20:13:38 +0700

    submit và chặn load lại trang

--- src/components/ComponentClass.js
+++ src/components/ComponentClass.js
@@ -8,8 +8,9 @@ class ComponentClass extends React.Component {
     postcode: 200,
   };
   //event là phải gõ đúng như sau vì react định nghĩa rồi
-  handleClick(event) {
-    console.log(event.target);
+
+  handleClick = (event) => {
+    console.log('Click me nhe!', event);
     console.log(
       'my sex is ',
       this.state.sex,
@@ -23,9 +24,9 @@ class ComponentClass extends React.Component {
       sex: 'Female',
       postcode: Math.floor(Math.random() * 100) + 1,
     });
-  }
+  };
   handleOnMouseOver(event) {
-    console.log(event);
+    console.log('Mouse Over me ne!', event);
     console.log(
       'my sex is ',
       this.state.sex,
@@ -34,25 +35,51 @@ class ComponentClass extends React.Component {
       'my zip code of province is',
       this.state.postcode
     );
-    this.setState({ sex: 'Female' });
+    this.setState({ sex: 'Male' });
     this.setState({ postcode: Math.floor(Math.random() * 100) + 1 });
     // console.log(event.pageX);
     // console.log(event.screenX);
   }
+  handleOnChangeInput = (event) => {
+    this.setState({
+      sex: event.target.value,
+    });
+    // alert('ok OnChangeInput');
+    // console.log('Xem tất cả thuộc tính của event tên OnChange, ', event);
+    // console.log(event.target.value);
+  };
+  /**
+   * @param {Event} event Đối tượng sự kiện.
+   */
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    //alert('ok OnSubmit');
+  };
   //jsx
   render() {
     return (
       <div>
         my sex is {this.state.sex}
+        <form
+          onSubmit={(event) => {
+            this.handleOnSubmit(event);
+          }}
+        >
+          <input
+            typ='text'
+            onChange={(event) => this.handleOnChangeInput(event)}
+          ></input>
+          <button type='submit'>Submit</button>
+        </form>
         <button
           onClick={(event) => {
             this.handleClick(event);
           }}
         >
-          click me nhe!
+          Click me nhe!
         </button>
         <button onMouseOver={(event) => this.handleOnMouseOver(event)}>
-          Mouse Over me nhe!
+          Mouse Over me ne!
         </button>
       </div>
     );
