10f8f31980e1e53110829e1856f448851a482caa
phamquangtrungangiang <phamquangtrungangiang@gmail.com>
2025-07-01 13:35:20 +0700

    compenent cha truyền props là một mảng các đối tượng cho component con có dùng map duyệt

--- src/components/ComponentClass.js
+++ src/components/ComponentClass.js
@@ -2,6 +2,14 @@ import React from 'react';
 import UserInfor from './UserInfor';
 import DisplayInfor from './DisplayInfor';
 class ComponentClass extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: 'Trung', age: 40, province: 'An giang' },
+      { id: 2, name: 'Quang', age: 40, province: 'An giang' },
+      { id: 3, name: 'Phạm', age: 40, province: 'An giang' },
+    ],
+  };
+
   //event là phải gõ đúng như sau vì react định nghĩa rồi
 
   // handleClick = (event) => {
@@ -39,14 +47,14 @@ class ComponentClass extends React.Component {
   //jsx
   render() {
     //tạo biến java script
-    const myarr = ['Vinh Long', 'Can Tho'];
-
+    // const myarr = ['Vinh Long', 'Can Tho'];
     return (
       <div>
-        <UserInfor />
-        <DisplayInfor sex='Famale' province='Dong Thap' postcode='200' />
+        <DisplayInfor listUsers={this.state.listUsers} />
+        {/* <UserInfor /> */}
+        {/* <DisplayInfor sex='Famale' province='Dong Thap' postcode='200' /> */}
         {/*dùng biến, và đặt trong cập ngoặc móc để truyền được nhiều dạng dữ liệu như số, chuỗi, mảng, đối tượng*/}
-        <DisplayInfor sex={'Male'} province={myarr} postcode={'500'} />
+        {/* <DisplayInfor sex={'Male'} province={myarr} postcode={'500'} /> */}
         {/* <button
           onClick={(event) => {
             this.handleClick(event);
--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,16 +1,29 @@
 import React from 'react';
+
 class DisplayInfor extends React.Component {
   //props, là đối tượng dùng truyền dữ liệu component cha cho component con
   render() {
-    console.log(this.props);
+    const { listUsers } = this.props;
+    // console.log('kiểm tra destruturing ', listUsers);
+
+    //console.log(this.props);
     // destructuring obj/array để tối ưu hóa code truy cập thuộc tính props
     // 3 biết lưu thuộc tính đối tượng props
-    const { sex, province, postcode } = this.props; 
+    //const { sex, province, postcode } = this.props;
     return (
       <div>
         <div>
-            {/* gõ tên biến, dùng biến thuộc tính của đối tượng props */}
-          my sex is {sex}, my province is {province}, my zip code is {postcode}
+          {listUsers.map((user) => {
+            console.log(user);
+            return (
+              <div key={user.id}>
+                my name's {user.name}my old's {user.age}my province's{' '}
+                {user.province}
+              </div>
+            );
+          })}
+          {/* gõ tên biến, dùng biến thuộc tính của đối tượng props */}
+          {/* my sex is {sex}, my province is {province}, my zip code is {postcode} */}
         </div>
       </div>
     );
